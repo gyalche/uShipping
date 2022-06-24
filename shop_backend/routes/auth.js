1f89c922e2d30a21d0453f9194c26430ef8a0458
Dawa <sherpadawa01@outlook.com>
2022-06-25 01:09:09 +0545

    work on progess

--- shop_backend/routes/auth.js
+++ shop_backend/routes/auth.js
@@ -1,12 +1,13 @@
 const router=require('express').Router();
-const user=require('../models/user')
+const user=require('../models/user');
+const CryptoJS = require('crypto-js');
 //REGISTER;
 
 router.post("/register", async (req, res)=>{
     const newUser=user({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
+        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
 
     });
    try{
@@ -15,6 +16,22 @@ router.post("/register", async (req, res)=>{
    }catch(e){
        res.status(500).json(e);
    }
+});
+
+//LOGIN;
+
+router.post("/login", async (req, res) => {
+    try{
+        const user=await user.findOne({username:username});
+        !user && res.status(401).json("Wrong username")
+        const hashedPassword=CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+        const password=hashedPassword.toString();
+
+        password!==req.body.password && res.status(401).json("Wrong password");
+        res.status(200).json(user);
+    }catch(e){
+        res.status(500).json(e);
+    }
 })
 
 
